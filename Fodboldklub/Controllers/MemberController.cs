dd5d5cbcdf285a0ba5e04deb143f030c8e58bd2c
Christian Anker Larsen <calanker@gmail.com>
2023-11-04 19:41:35 +0100

    Added new controller and View template for members

---
+++ Fodboldklub/Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fodboldklub.Controllers
+{
+    public class MemberController : Controller
+    {
+        public string Index()
+        {
+            return "hello world!";
+        }
+    }
+}
