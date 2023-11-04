742d228a91e774a8e928a4bf2ef99dfd6bbd0352
Christian Anker Larsen <calanker@gmail.com>
2023-11-04 22:00:13 +0100

    Added new modelse

---
+++ Fodboldklub/Controllers/MembersController.cs
@@ -0,0 +1,22 @@
+﻿using Fodboldklub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fodboldklub.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult List()
+        {
+            var membersViewModel = new MembersViewModel
+            {
+                Members = new List<Member>()
+            };
+
+            return View(membersViewModel);
+        }
+    public IActionResult Add()
+    {
+        return View();
+    }
+}
+}
---
+++ Fodboldklub/Models/MembersViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Fodboldklub.Models
+{
+    public class MembersViewModel
+    {
+        public List<Member>? Members { get; set; }
+        public bool IsMembersListEmpty { get; set; }
+    }
+}
