062d979fa378a53f1dd8a25b1044d89a41d026c0
Christian Anker Larsen <calanker@gmail.com>
2023-11-05 14:26:44 +0100

    Added session storage

--- Fodboldklub/Controllers/MembersController.cs
+++ Fodboldklub/Controllers/MembersController.cs
@@ -1,4 +1,5 @@
-﻿using Fodboldklub.Models;
+﻿using System.Text.Json;
+using Fodboldklub.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fodboldklub.Controllers
@@ -7,16 +8,43 @@ namespace Fodboldklub.Controllers
     {
         public IActionResult List()
         {
-            var membersViewModel = new MembersViewModel
-            {
-                Members = new List<Member>()
-            };
+            var membersJson = HttpContext.Session.GetString("Members");
+            var members = membersJson != null ? JsonSerializer.Deserialize<List<Member>>(membersJson) : new List<Member>();
 
+            var membersViewModel = new MembersViewModel { Members = members };
             return View(membersViewModel);
         }
-    public IActionResult Add()
-    {
-        return View();
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Member inputMember)
+        {
+            if (ModelState.IsValid)
+            {
+                var membersJson = HttpContext.Session.GetString("Members");
+                var members = membersJson != null ? JsonSerializer.Deserialize<List<Member>>(membersJson) : new List<Member>();
+
+                Member newMember = new Member
+                {
+                    FirstName = inputMember.FirstName,
+                    LastName = inputMember.LastName,
+                    Address = inputMember.Address,
+                    Phone = inputMember.Phone,
+                    Email = inputMember.Email,
+                };
+                members.Add(newMember);
+                Console.WriteLine($"{newMember.FirstName} {newMember.LastName} added.");
+                foreach (Member member in members)
+                {
+                    Console.WriteLine($"{member.FirstName} {member.LastName}");
+                }
+                HttpContext.Session.SetString("Members", JsonSerializer.Serialize(members));
+            }           
+            return RedirectToAction("List");
+        }
+
     }
 }
-}
+
